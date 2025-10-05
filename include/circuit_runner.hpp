ecd1f44a6a7e32f4d0ddc794626f2741c1ab80a0
SegusFaultise <wilsonzakk8@gmail.com>
2025-10-05 10:01:39 +1000

    bye bye old code.......... bye

--- include/circuit_runner.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef QUANTUM_RUNNER_HPP
-#define QUANTUM_RUNNER_HPP
-
-#include "simulation_options.hpp"
-#include "state.hpp"
-
-void run_circuit(QuantumState &state, const CircuitOptions &options);
-
-#endif // !QUANTUM_RUNNER_HPP
